87d058f2d1becb168d70af95e7e1f2a9a89e5a6d
Ivan Sugerman <ivan@ivansugerman.com>
2013-10-07 21:31:30 -0400

    changed jquery cdn

--- test/index.php
+++ test/index.php
@@ -3,7 +3,7 @@
     <head>
         <title>jquery.autoBackground.js</title>
         
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script type="text/javascript" src="../jquery.autoBackground.js"></script>
     </head>
     <body>
